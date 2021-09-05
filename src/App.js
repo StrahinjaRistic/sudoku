22bce0c068467924d557c520f26db3c1cfbfe17d
Strahinja_R <st_raja@hotmail.com>
2021-09-06 01:41:12 +0200

    table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import Header from 'components/layout/Header';
-import Footer from 'components/layout/Footer';
+import SudokuGame from 'SudokuGame';
 
-import styled from 'styled-components';
 import GlobalStyle from 'theme/globalStyles';
-
-const Container = styled.div`
-  max-width: 700px;
-  min-width: 270px;
-  margin: 0 auto;
-  padding: 0 10px;
-`;
 
 const App = () => {
   return (
-    <Container>
+    <>
       <GlobalStyle />
-      <Header />
-      <Footer />
-    </Container>
+      <SudokuGame />
+    </>
   );
 };
 
---
+++ src/components/layout/SudokuTable.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Game = styled.section`
+  width: 70%;
+`;
+
+const GameTable = styled.table`
+  font-family: 'Noto Sans', sans-serif;
+  font-size: 26px;
+  margin: 30px 0;
+  border: 2px solid var(--color-grey);
+  border-collapse: collapse;
+`;
+
+const TableRow = styled.tr`
+  &:nth-child(3n) {
+    border-bottom: 2px solid var(--color-grey);
+  }
+`;
+const TableCell = styled.td`
+  border: 1px solid var(--color-grey-lighter);
+  padding: 22px 23px;
+  color: var(--background-color);
+  cursor: pointer;
+  &:nth-child(3n) {
+    border-right: 2px solid var(--color-grey);
+  }
+`;
+
+const SudokuTable = () => {
+  const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const gameArray = [];
+  return (
+    <Game>
+      <GameTable>
+        <tbody>
+          {rows.map((row) => {
+            return (
+              <TableRow key={row}>
+                {rows.map((column) => {
+                  const indexOfArray = row * 9 + column;
+                  const value = gameArray[indexOfArray];
+                  return <TableCell></TableCell>;
+                })}
+              </TableRow>
+            );
+          })}
+        </tbody>
+      </GameTable>
+    </Game>
+  );
+};
+
+export default SudokuTable;
