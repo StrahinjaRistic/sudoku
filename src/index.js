072d785d50db860af3535033b284efb72b4df3c2
Strahinja_R <st_raja@hotmail.com>
2021-09-12 04:24:04 +0200

    table-sudoku

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { GameContextProvider } from 'context/game-context';
+
 ReactDOM.render(
-  <>
+  <GameContextProvider>
     <App />
-  </>,
+  </GameContextProvider>,
   document.getElementById('root')
 );
